f36e604126e520cad68349e6ea29188986e12a6a
Jon Evans <jle@foraker.com>
2015-03-06 14:59:34 -0700

    Allow deleting friends

--- app/routes/friends.js
+++ app/routes/friends.js
@@ -1,7 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function() {
-    return this.store.find('friend');
+  actions: {
+    delete: function(friend) {
+      var _this = this;
+
+      friend.destroyRecord().then(function() {
+        _this.transitionTo('friends.index');
+      });
+
+      return false;
+    }
   }
 });
